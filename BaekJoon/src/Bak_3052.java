12566295ad30d07841eb937d67fe8eeb3c79cec3
Yoodong777 <youwoa7@daum.net>
2023-10-25 23:14:51 +0900

    second project

---
+++ BaekJoon/src/Bak_3052.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Bak_3052 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] num = new int[10];
+        int count = 0;
+        for (int i = 0; i < 10; i++) {
+            num[i] = sc.nextInt() % 42;
+        }
+        for (int i = 0; i < 10; i++) {
+            int temp = 0;
+            for (int j = i + 1; j < 10; j++) {
+                if(num[i] == num[j]){
+                    temp ++;
+                }
+            }
+            if(temp == 0){
+                count ++;
+            }
+        }
+        System.out.println(count);
+
+    }
+}
---
+++ BaekJoon/src/TEST2.java
@@ -0,0 +1,26 @@
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.Buffer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.util.StringTokenizer;
+
+public class TEST2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for (int i = 0; i < a; i++) {
+            st = new StringTokenizer(br.readLine());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            bw.write(b + c + " ");
+        }
+        bw.flush();
+
+    }
+}
