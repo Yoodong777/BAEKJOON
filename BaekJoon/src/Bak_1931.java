7477c980239087d556a7c2ef038a304bae378c72
Yoodong777 <youwoa7@daum.net>
2023-10-30 00:58:30 +0900

    1931번 실패

---
+++ BaekJoon/src/Bak_1931.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class Bak_1931 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int count = 0;
+
+        int[][] arr= new int[a][2];
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < 2; j++) {
+                if(arr[i][1] <= arr[][] ){
+
+                }
+            }
+        }
+
+
+    }
+
+
+
+
+}
+
+
+
+
+/*
+11 의미가 없어
+1 4 4--> 큰놈를 찾아 첫번째 자리 count++ 올려버려
+그 count를 올린놈의 숫자를 저장해 그 숫자의 또 두번째 자리랑
+다른놈이랑 비교해 그놈보다 큰놈이 있으면 count++ 올려
+
+이걸 계속 비교함
+
+
+ */
