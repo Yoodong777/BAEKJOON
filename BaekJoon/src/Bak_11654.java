ba2fb7b3ffb861c589b4d6726facea0069f51323
Yoodong777 <youwoa7@daum.net>
2023-11-05 22:43:13 +0900

    거의 회복 후 공부

---
+++ BaekJoon/src/Bak_11654.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Bak_11654 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String st = sc.next();
+        int result = st.charAt(0);
+
+        System.out.println(result);
+    }
+}
---
+++ BaekJoon/src/Bak_2444.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Bak_2444 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        for (int i = 1; i <= a; i++) {
+            for (int j = 0; j < a - i; j++)
+                System.out.print(" ");
+            for (int j = 0; j < i * 2 - 1; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+
+        for (int i = a - 1; i >= 0; i--) {
+            for (int j = 0; j < a - i; j++)
+                System.out.print(" ");
+            for (int j = 0; j < i * 2 - 1; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
