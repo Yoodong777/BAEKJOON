8bf5e763437cd2bc73465ec1158aafed1483cd1c
Yoodong777 <youwoa7@daum.net>
2023-11-09 20:58:08 +0900

    20231109

---
+++ BaekJoon/src/Bak_1316.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Bak_1316 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int count = 0;
+
+        for (int i = 0; i < a; i++) {
+            String st = sc.next();
+            char c = st.charAt(i);
+
+            if(st.charAt(i) == st.charAt(i + 1)){
+                System.out.println("1입니다");
+            }
+            if( st.charAt(i) )
+
+
+        }
+    }
+
+}
