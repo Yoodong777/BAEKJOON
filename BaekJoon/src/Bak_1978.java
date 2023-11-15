0f9dbac3a04d0237833362b8bcf335e472625909
Yoodong777 <youwoa7@daum.net>
2023-11-16 00:51:45 +0900

    20231116

--- BaekJoon/src/Bak_1978.java
+++ BaekJoon/src/Bak_1978.java
@@ -4,8 +4,20 @@ public class Bak_1978 {
 
         Scanner sc = new Scanner(System.in);
 
+        int a = sc.nextInt();
+        int count = 0;
 
+        for (int i = 0; i < a; i++) {
+            int b = sc.nextInt();
+            for (int j = 2; j <= b; j++) {
+                if(j == b){
+                    count++;
+                }if(b % j == 0){
+                    break;
+                }
 
-
+            }
+        }
+        System.out.println(count);
     }
 }
---
+++ BaekJoon/src/Bak_2581.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Bak_2581 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        int[] array = new int[N];
+        int sum = 0;
+        int index = 0;
+
+        for (int i = M; i <= N; i++) {
+            for (int j = 2; j <= i; j++) {
+                if(j == i){
+                    sum+= i;
+                    array[index] = i;
+                    index++;
+                }if(i % j == 0){
+                    break;
+                }
+            }
+        }
+        if(sum == 0){
+            System.out.println(-1);
+        }if(sum > 0){
+            System.out.println(sum);
+            System.out.println(array[0]);
+        }
+    }
+}
