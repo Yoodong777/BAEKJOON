6be4be33d62dd3ca675c2f7f3fc6582f2a2db2a8
Yoodong777 <youwoa7@daum.net>
2023-11-19 23:17:28 +0900

    20231119

--- BaekJoon/src/Bak_1157.java
+++ BaekJoon/src/Bak_1157.java
@@ -1,21 +1,35 @@
-import javax.imageio.metadata.IIOMetadataFormatImpl;
 import java.util.*;
 public class Bak_1157 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
+        int[] arr = new int[26];
         String st = sc.next();
 
         for (int i = 0; i < st.length(); i++) {
-            char c = st.charAt(i);
-        }
-        for (int i = 0; i < st.length(); i++) {
-            if(st.charAt(i) == 'i'){
-                System.out.println();
+
+            if('A' <= st.charAt(i) && st.charAt(i) <= 'Z'){
+                arr[st.charAt(i) - 'A']++;
             }
 
-            /* 실패..*/
+            else {
+                arr[st.charAt(i) - 'a']++;
+            }
         }
+
+        int max = -1;
+        char ch = '?';
+
+        for (int i = 0; i < 26; i++) {
+            if(arr[i] > max){
+                max = arr[i];
+                ch = (char) (i + 65);
+            }
+            else if(arr[i] == max){
+                ch = '?';
+            }
+        }
+        System.out.println(ch);
     }
 }
