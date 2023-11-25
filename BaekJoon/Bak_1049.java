a581d20a53d27e501494d5a26501fc7397243b78
Yoodong777 <youwoa7@daum.net>
2023-11-26 03:57:06 +0900

    20231126

---
+++ BaekJoon/Bak_1049.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Bak_1049 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int M = scan.nextInt();
+        int payMoney = 0;
+        int[] pack = new int[M];
+        int[] unit = new int[M];
+        for(int i=0; i<M; i++) {
+            pack[i] = scan.nextInt();
+            unit[i] = scan.nextInt();
+        }
+
+        Arrays.sort(pack);
+        Arrays.sort(unit);
+
+        payMoney = Math.min(((N/6) + 1) * pack[0], Math.min(N * unit[0], ((N/6) * pack[0]) + ((N%6) * unit[0])));
+
+        System.out.println(payMoney);
+        scan.close();
+    }
+
+}
