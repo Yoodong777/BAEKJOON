b49d421bfa5ac20005d10594eeecc12ea611f06f
Yoodong777 <youwoa7@daum.net>
2023-12-12 01:09:04 +0900

    20231212

--- src/ObjectStudy/ObjectStudy_1.java
+++
@@ -1,8 +0,0 @@
-package ObjectStudy;
-
-public class ObjectStudy_1 {
-    public static void main(String[] args) {
-        System.out.println(10 + 20);
-        System.out.println(20 + 40);
-    }
-}
