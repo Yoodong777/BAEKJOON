603cfc0bc4c7d351bcbc02730f68b3b39fac8bdb
Yoodong777 <youwoa7@daum.net>
2023-12-28 21:05:26 +0900

    20231228 Bak_1063

---
+++ src/BaekJoon/Bak_1063.java
@@ -0,0 +1,79 @@
+package BaekJoon;
+
+import java.util.*;
+
+public class Bak_1063 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String king = sc.next();
+        int kingX = king.charAt(0) - 'A' + 1;
+        int kingY = king.charAt(1) - '0';
+
+        String stone = sc.next();
+
+        int stoneX = stone.charAt(0) - 'A' + 1;
+        int stoneY = stone.charAt(1) - '0';
+
+        int[] dx = {1, -1, 0, 0, 1, -1, 1, -1};
+        int[] dy = {0, 0, -1, 1, 1, 1, -1, -1};
+
+
+        int n = Integer.parseInt(sc.next());
+        for (int i = 0; i < n; i++) {
+            String move = sc.next();
+            int idx = 0;
+            int tmpX = kingX, tmpY = kingY;
+            switch (move) {
+                case "R":
+                    idx = 0;
+                    break;
+                case "L":
+                    idx = 1;
+                    break;
+                case "B":
+                    idx = 2;
+                    break;
+                case "T":
+                    idx = 3;
+                    break;
+                case "RT":
+                    idx = 4;
+                    break;
+                case "LT":
+                    idx = 5;
+                    break;
+                case "RB":
+                    idx = 6;
+                    break;
+                case "LB":
+                    idx = 7;
+                    break;
+            }
+            tmpX += dx[idx];
+            tmpY += dy[idx];
+
+            if (tmpX >= 1 && tmpX <= 8 && tmpY >= 1 && tmpY <= 8) {
+                if (tmpX == stoneX && tmpY == stoneY) {
+                    if (stoneX + dx[idx] >= 1 && stoneX + dx[idx] <= 8 && stoneY + dy[idx] >= 1 && stoneY + dy[idx] <= 8) {
+                        stoneX += dx[idx];
+                        stoneY += dy[idx];
+                    } else {
+                        continue;
+                    }
+                }
+                kingX = tmpX;
+                kingY = tmpY;
+            }
+        }
+        king = "";
+        stone = "";
+
+        king += Character.toString((char) (kingX + 'A' - 1)) + kingY;
+        stone += Character.toString((char) (stoneX + 'A' - 1)) + stoneY;
+
+        System.out.println(king);
+        System.out.println(stone);
+    }
+
+}
